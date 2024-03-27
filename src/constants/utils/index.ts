5fa067fd9b79c12247a2a47c16d57322c5369162
nileshgalaxy <nilesh.patel@galaxyweblinks.in>
2024-03-27 13:09:08 +0530

    minor project

---
+++ src/constants/utils/index.ts
@@ -0,0 +1,15 @@
+export const HEADER_TITLE_CENTER = 'center';
+
+export const ASYNC_CONST = {
+  LoggedInUserInfo: 'LoggedInUserInfo',
+  LANGUAGE_KEY: 'LANGUAGE_KEY',
+};
+
+/**
+ * align content
+ */
+export const ALIGNMENT = {
+  left: 'left',
+  right: 'right',
+  center: 'center',
+};
---
+++ src/redux/store.ts
@@ -0,0 +1,17 @@
+import {configureStore} from '@reduxjs/toolkit';
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
+import {authApi} from './api/authApi';
+
+export const store = configureStore({
+  reducer: {
+    [authApi.reducerPath]: authApi.reducer,
+  },
+  devTools: true,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({}).concat([authApi.middleware]),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
